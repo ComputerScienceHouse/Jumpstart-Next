1e7a53b535ad18fc0a849f21760df90e436dc7fa
iTecAI <matteovh@gmail.com>
2021-10-02 22:24:56 -0400

    fix datadog display bug @ higher resolutions

--- web/src/components/datadog.js
+++ web/src/components/datadog.js
@@ -4,15 +4,19 @@ import { PanelHeader } from "./componentUtil.js";
 
 export function PanelDataDog() {
     return (
-        <div className="panel datadog shadow" style={{ gridArea: "datadog" }}>
+        <div className="panel datadog shadow" style={{ gridArea: "datadog", overflow: 'hidden' }}>
             <PanelHeader title="DataDog - Server Management" />
-            <svg className="frame" viewBox="0 0 1080 810">
+            <svg className="frame" viewBox="0 0 1080 810" style={{
+                height: '100%',
+                overflow: 'hidden',
+                width: 'calc(100% + 30px)'
+            }}>
                 <foreignObject x="0" y="0" width="1080" height="1000">
                     <iframe
                         src="https://p.datadoghq.com/sb/44f16805b-cab847d58c"
                         style={{
                             width: "100%",
-                            height: "calc(100% - 32px + 175px)",
+                            height: "calc(100% + 100px)",
                             border: "none",
                             marginTop: "-90px",
                             overflow: 'hidden',
