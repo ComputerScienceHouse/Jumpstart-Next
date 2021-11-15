e6dc333f67bff8e9e722ffcd446eeeba07fe97a8
iTecAI <matteovh@gmail.com>
2021-11-15 09:50:36 -0500

    basic light & dark themes

--- web/src/App.js
+++ web/src/App.js
@@ -6,21 +6,27 @@ import PanelDataDog from "./components/datadog.js";
 import PanelCalendar from "./components/calendar.js";
 import PanelAnnouncements from "./components/announcements";
 import PanelLogo from "./components/logo";
-import { updateWebSocket } from './util';
+import { updateWebSocket } from "./util";
 
 function App() {
-    useEffect(function () {
-      return () => updateWebSocket.close();
-    });
-    return <div className="app-root">
+  useEffect(function () {
+    return () => updateWebSocket.close();
+  });
+  return (
+    <div className="app-root">
       <PanelLogo />
       <PanelInfo />
-      <div className="panel ticker shadow" style={{gridArea: 'ticker'}}> </div>
+      <div className="panel ticker shadow" style={{ gridArea: "ticker" }}>
+        {" "}
+      </div>
       <PanelAnnouncements />
       <PanelCalendar />
       <PanelDataDog />
-      <div className="panel food shadow" style={{gridArea: 'food'}}> </div>
-    </div>;
+      <div className="panel food shadow" style={{ gridArea: "food" }}>
+        {" "}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- web/src/components/logo.js
+++ web/src/components/logo.js
@@ -1,44 +1,49 @@
 import React from "react";
 import "../App.scss";
-import { useEffect, useState, useLayoutEffect } from "react";
-
-var themeOverride = null;
-var lastTheme = null;
+import { useEffect, useState } from "react";
 
 function getCurrentTheme() {
-    if (themeOverride) { return themeOverride; } else {
-        var date = new Date(Date.now());
-        if (date.getHours() >= 23 || date.getHours() < 7) {
-            return 'base_dark'; // Set dark mode @ night
-        } else {
-            return 'base_dark'
-        }
-    }
+  var date = new Date(Date.now());
+  if (date.getHours() >= 23 || date.getHours() < 7) {
+    return "base_dark"; // Set dark mode @ night
+  } else {
+    return "base_light";
+  }
 }
 
 export function PanelLogo() {
-    const [theme, setTheme] = useState('base_dark');
+  const [updateRandom, setUpdateRandom] = useState(0);
+  function themeInterval() {
+    fetch("/api/themes/style?r=" + Math.random(), {
+      cache: "no-store",
+    }).then(function (r) {
+      r.json().then(function (j) {
+        document.body.setAttribute(
+          "style",
+          Object.entries(j)
+            .map(([k, v]) => `--${k}:${v}`)
+            .join(";")
+        );
+        setUpdateRandom(Math.random());
+      });
+    });
+  }
 
-    useEffect(function () {
-        var themeUpdateInterval = setInterval(function () {
-            var currentTheme = getCurrentTheme();
+  useEffect(function () {
+    var themeUpdateInterval = setInterval(themeInterval, 2000);
+    themeInterval();
+    return () => clearInterval(themeUpdateInterval);
+  }, []);
 
-            if (currentTheme != lastTheme) {
-                lastTheme = currentTheme;
-                setTheme(currentTheme);
-                fetch('/themes/'+theme+'/style.json').then(function (r) { r.json().then(function (j) {
-                    document.body.setAttribute('style', Object.entries(j).map(([k, v]) => `--${k}:${v}`).join(';'));
-                })});
-            }
-        }, 1000);
-        return () => clearInterval(themeUpdateInterval);
-    }, []);
-
-    return (
-        <div className="logo" style={{ gridArea: "logo", overflow: 'hidden' }}>
-            
-        </div>
-    );
+  return (
+    <div className="logo" style={{ gridArea: "logo", overflow: "hidden" }}>
+      <img
+        src={"/api/themes/logo?r=" + updateRandom}
+        alt="CSH Logo"
+        className="logo-img"
+      />
+    </div>
+  );
 }
 
 export default PanelLogo;
