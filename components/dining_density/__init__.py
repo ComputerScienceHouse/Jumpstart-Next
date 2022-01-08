64beaaa373819a16598ab0a13c5303c0fa9198d6
iTecAI <matteovh@gmail.com>
2022-01-08 17:22:57 -0500

    fixes

--- components/dining_density/__init__.py
+++ components/dining_density/__init__.py
@@ -8,46 +8,55 @@ import requests
 import threading
 from component_util import post_update
 
-conf = json.loads(os.environ['CONFIG'])
+conf = json.loads(os.environ["CONFIG"])
 mongo_client = MongoClient(
-    host=conf['mongodb']['host'],
-    port=conf['mongodb']['port'],
-    username=conf['mongodb']['username'],
-    password=conf['mongodb']['password'],
-    tls=conf['mongodb']['tls']
+    host=conf["mongodb"]["host"],
+    port=conf["mongodb"]["port"],
+    username=conf["mongodb"]["username"],
+    password=conf["mongodb"]["password"],
+    tls=conf["mongodb"]["tls"],
 )
-db = mongo_client[conf['mongodb']['database']]
+db = mongo_client[conf["mongodb"]["database"]]
 
-class OpenFoodWrapper:    
+
+class OpenFoodWrapper:
     def __init__(self, update=300) -> None:
         self.update = update
 
     def start(self):
-        threading.Thread(name="thread_jumpstart-next_component_dining-density_food", target=self.fetchLoop, daemon=True).start()
-        info('Started dining-density.food thread')
+        threading.Thread(
+            name="thread_jumpstart-next_component_dining-density_food",
+            target=self.fetchLoop,
+            daemon=True,
+        ).start()
+        info("Started dining-density.food thread")
 
     def fetchLoop(self):
         while True:
             try:
-                data = requests.get('https://maps.rit.edu/proxySearch/densityMap.php').json()['busyness']
-                data = data[:17] # Food places are from 0-16, unfortunate hardcoding...update later
-                for i in range(len(data)): 
-                    data[i] = data[i]['properties']
-                    data[i]['pct_full'] = int(100*data[i]['count']/data[i]['max_occ'])
-                    data[i].pop("mdo_id")                    
+                data = requests.get(
+                    "https://maps.rit.edu/proxySearch/densityMap.php"
+                ).json()["busyness"]
+                data = data[
+                    :
+                ]  # Food places are from 0-16, unfortunate hardcoding...update later
+                for i in range(len(data)):
+                    data[i] = data[i]["properties"]
+                    data[i]["pct_full"] = int(
+                        100 * data[i]["count"] / data[i]["max_occ"]
+                    )
+                    data[i].pop("mdo_id")
                     data[i].pop("open_status")
-                    data[i].pop("max_occ")      
+                    data[i].pop("max_occ")
                 # data is now like this
                 # {0: {"name": name, "count": count, "pct_full": pct_full}, 1: ...}
-                data = {
-                    'data': data,
-                    'record': 'food'
-                }
-                db.dining_density.replace_one({'record': 'food'}, data, upsert=True)
-                post_update('dining-density.food')
+                data = {"data": data, "record": "food"}
+                db.dining_density.replace_one({"record": "food"}, data, upsert=True)
+                post_update("dining-density.food")
             except:
-                exception('Failed to fetch food data: ')
+                exception("Failed to fetch food data: ")
             time.sleep(self.update)
+
 
 """    @app.get('/api/food')
     async def get_food():
@@ -66,19 +75,16 @@ class OpenFoodWrapper:
             return {"message": "error"} """
 
 DiningDensityComponentRouter = APIRouter(
-    prefix='/api/components/dining-density', 
-    tags=['component', 'dining-density']        
+    prefix="/api/components/dining-density", tags=["component", "dining-density"]
 )
 
-@DiningDensityComponentRouter.get('/food')
+
+@DiningDensityComponentRouter.get("/food")
 async def get_food(r: Response):
-    food_data = db.dining_density.find_one(filter={'record': 'food'})
+    food_data = db.dining_density.find_one(filter={"record": "food"})
     if food_data:
-        del food_data['_id']
-        return {
-            'result': 'success',
-            'data': food_data
-        }
+        del food_data["_id"]
+        return {"result": "success", "data": food_data}
     else:
         r.status_code = HTTP_404_NOT_FOUND
-        return {'result': 'failed: food record not generated'}
+        return {"result": "failed: food record not generated"}
